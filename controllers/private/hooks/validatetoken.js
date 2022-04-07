36fc94f497eb53eb775cac4d4787ccd18f5fd3c3
Carlo Cuya <carlo368723@hotmail.com>
2022-04-07 17:04:20 -0500

    fix(validationToken): Se agrega validacion para las url que no tienen cuerpo de solicitud Authorization ahora deben enviar el token

--- controllers/private/hooks/validatetoken.js
+++ controllers/private/hooks/validatetoken.js
@@ -1,28 +1,43 @@
+var jwt = require('jsonwebtoken')
+
 module.exports = function (req, reply, done) {
     const exclude = [
         '/documentation'
     ]
     req.jwtVerify(function (err, decoded) {
-        try {
-            if (decoded) {
+        const { token } = req.query
+        if (token) {
+            try {
+                jwt.verify(token, process.env.jwtSecretPass)
                 done()
-            } else {
-                let pass = false
-                for(let i = 0; i < exclude.length; i++) {
-                    if (new RegExp(`^${exclude[i]}`).test(req.url)) {
-                        pass = true
-                        break
-                    }
-                }
-                if (pass) {
+            } catch (e) {
+                reply.send({
+                    statusCode: 401,
+                    error: "Unauthorized",
+                    message: "Token de autorización es inválido."
+                })
+            }
+        } else {
+            try {
+                if (decoded) {
                     done()
                 } else {
-                    reply.send(err);
+                    let pass = false
+                    for(let i = 0; i < exclude.length; i++) {
+                        if (new RegExp(`^${exclude[i]}`).test(req.url)) {
+                            pass = true
+                            break
+                        }
+                    }
+                    if (pass) {
+                        done()
+                    } else {
+                        reply.send(err)
+                    }
                 }
+            } catch (e) {
+                reply.send(e)
             }
-        } catch (e) {
-            console.log("ERROR")
-            reply.send(e);
         }
-    });
+    })
 }
