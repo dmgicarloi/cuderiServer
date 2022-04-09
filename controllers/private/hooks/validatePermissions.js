24113a290331e61cddba815ad8a78c65676a56f9
Carlo Cuya <carlo368723@hotmail.com>
2022-04-09 18:38:12 -0500

    feat(validatePermission): Se valida permisos de ruta

---
+++ controllers/private/hooks/validatePermissions.js
@@ -0,0 +1,27 @@
+module.exports = async function (req, reply) {
+    const ruta = req.headers.ruta
+    if (!ruta && process.env.dev === 'true') {
+        return
+    }
+    const menuUsuarioService = this.getService('MenuUsuarioService')
+    const { id_usuario } = await this.$info(req)
+    const error = { message: 'No tienes permisos suficientes', statusCode: 'No Action', code: 'noAction' }
+    if (!req.headers.ruta) {
+        const menus = await menuUsuarioService.getMenus(id_usuario)
+        error.menus = menus
+        error.permission = null
+        reply.code(401)
+        reply.send(error)
+    }
+    let { id_permiso, metodo } = (await menuUsuarioService.validateMenuUsuario({ id_usuario, ruta: req.headers.ruta })) || {}
+    id_permiso = id_permiso || -1
+    metodo = metodo || ''
+    const methods = metodo.split(',')
+    if (!methods.includes(req.method)) {
+        const menus = await menuUsuarioService.getMenus(id_usuario)
+        error.menus = menus
+        error.permission = id_permiso === -1 ? null : id_permiso
+        reply.code(401)
+        reply.send(error)
+    }
+}
