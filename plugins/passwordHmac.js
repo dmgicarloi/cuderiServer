24e1fdf48d6b38bdabf64a982446c1cbee73fcee
Carlo <carlo368723@gmail.com>
2022-05-07 12:02:30 -0500

    feat(passwordHmac): Se agrega archivo para encriptar un texto base 64 a hmac sha256

---
+++ plugins/passwordHmac.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const fp = require("fastify-plugin")
+const crypto = require('crypto')
+
+module.exports = fp(async function (_this, opts) {
+    _this.decorate("passwordHmac", function (passwordBase64) {
+        let password = new Buffer.from(passwordBase64, 'base64')
+        password = crypto
+                .createHmac('sha256', process.env.secretPassword)
+                .update(decodeURIComponent(password.toString('ascii')))
+                .digest('hex')
+    })
+    return password
+})
