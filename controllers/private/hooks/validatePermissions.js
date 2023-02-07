c96c209ab0897abf1caf855b476c193ba1cb2bd3
Carlo <carlo368723@gmail.com>
2023-02-06 19:37:39 -0500

    fix(validatePermissions): Se valida hook de validacion de permisos

--- controllers/private/hooks/validatePermissions.js
+++ controllers/private/hooks/validatePermissions.js
@@ -4,21 +4,21 @@ module.exports = async function (req, reply) {
         return
     }
     const menuUsuarioService = this.getService('MenuUsuarioService')
-    const { id_usuario, id_tipo_socio, id_organizacion, id_empresa, id_sede } = await this.$info(req)
+    const { id_usuario, id_tipo_socio, id_organizacion, id_empresa, id_sede, id_almacen } = await this.$info(req)
     const error = { message: 'No tienes permisos suficientes', statusCode: 'No Action', code: 'noAction' }
     if (!req.headers.ruta) {
-        const menus = await menuUsuarioService.getMenus(id_usuario, id_tipo_socio, id_organizacion, id_empresa, id_sede)
+        const menus = await menuUsuarioService.getMenus(id_usuario, id_tipo_socio, id_organizacion, id_empresa, id_sede, id_almacen)
         error.menus = menus
         error.permission = null
         reply.code(401)
         reply.send(error)
     }
-    let { id_permiso, metodo } = (await menuUsuarioService.validateMenuUsuario({ id_usuario, id_tipo_socio, id_organizacion, id_empresa, id_sede, ruta: req.headers.ruta })) || {}
+    let { id_permiso, metodo } = (await menuUsuarioService.validateMenuUsuario({ id_usuario, id_tipo_socio, id_organizacion, id_empresa, id_sede, id_almacen, ruta: req.headers.ruta })) || {}
     id_permiso = id_permiso || -1
     metodo = metodo || ''
     const methods = metodo.split(',')
     if (!methods.includes(req.method)) {
-        const menus = await menuUsuarioService.getMenus(id_usuario, id_tipo_socio, id_organizacion, id_empresa, id_sede)
+        const menus = await menuUsuarioService.getMenus(id_usuario, id_tipo_socio, id_organizacion, id_empresa, id_sede, id_almacen)
         error.menus = menus
         error.permission = id_permiso === -1 ? null : id_permiso
         reply.code(401)
